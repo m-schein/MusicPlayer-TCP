e2bc25a9f75578e987d4451206486e7ae389ffab
Eduarda Waechter <eduarda.waechter@sap.com>
2023-03-25 12:19:37 -0300

    fix

--- src/commandActions/AddMusicalNoteAction.java
+++ src/commandActions/AddMusicalNoteAction.java
@@ -7,7 +7,7 @@ import enums.SoundCommands;
 public class AddMusicalNoteAction implements CommandAction {
     @Override
     public void executeSoundCommand(SoundControl soundControl, SoundCommands command, String input) {
-        String musicalNote = command.getValue().replace('(', ' ').replace(')', ' ').trim();
+        String musicalNote = input;
         soundControl.getVocals().add(musicalNote);
         soundControl.setPreviousMusicalNote(musicalNote);
     }
